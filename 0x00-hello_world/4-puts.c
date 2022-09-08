e1601a44587bd9d8818122ac6afd4868e380f52b
cj1607 <ucheokorojefferson@gmail.com>
2022-09-08 11:50:53 -0700

    puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-    char text[52] = "\"Programming is like building a multilingual puzzle";
-  
-    puts(text);
-  
-    return (0);  
+        char text[52] = "\"Programming is like building a multilingual puzzle";
+
+	puts(text);
+
+	return (0);
 }
