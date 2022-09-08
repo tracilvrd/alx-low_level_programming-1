e201bfb0f171f3965e347376cab3e06f97229f63
cj1607 <ucheokorojefferson@gmail.com>
2022-09-08 14:24:30 -0700

    puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - Program entry point
-*
-* Return: 0 if succesful, 1 otherwise
-*/
+ * main - Program entry point
+ *
+ * Return: 0 if successful, 1 otherwise
+ */
 int main(void)
 {
-	char text[52] = "\" Preogramming is like building a multilingual puzzle";
+  	char text[52] = "\"Programming is like building a multilingual puzzle";
+
 	puts(text);
 
 	return (0);
