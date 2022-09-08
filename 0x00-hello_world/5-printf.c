79eb272ad39a155a95eef9f9e8652bd90cef5bcc
cj1607 <ucheokorojefferson@gmail.com>
2022-09-08 07:04:32 -0700

    printf c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+  /* Print the required text using printf function */
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+
+  return (0);
+}
