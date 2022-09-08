f04f3faf29613bf12efc02296dd5e1603d0044e2
cj1607 <ucheokorojefferson@gmail.com>
2022-09-08 06:40:45 -0700

    puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Program entry point
+
+ *
+
+ * Return: 0 if successful, 1 otherwise
+
+ */
+
+int main(void)
+  
+{
+  
+  char text[52] = "\"Programming is like building a multilingual puzzle";
+  
+
+  
+  puts(text);
+  
+
+  
+  return (0);
+  
+}
