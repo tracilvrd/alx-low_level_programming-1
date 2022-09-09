8d46f6e6590cf0b8f6c2742cecfeb724d78f392c
cj1607 <ucheokorojefferson@gmail.com>
2022-09-09 12:23:25 -0700

    print t

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point. Prints alphabets in lowercase but reversed
+ *
+ * Return: 0 id successful. Non-zero otherwise
+ */
+int main(void)
+{
+  	int c = 122;
+
+	while (c >= 97)
+	{
+	  	putchar(c);
+		c--;
+	}
+	putchar(0xA);
+	return (0);
+}
