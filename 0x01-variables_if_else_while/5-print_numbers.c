546e38377428de3d5fbdffa4cb2cf316c5ba7e18
cj1607 <ucheokorojefferson@gmail.com>
2022-09-09 12:05:33 -0700

    print n
    /n

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point. Prints alphabets in lowercase excluding q and e
+ *
+ * Return: 0 if successful. Non-zero otherwise
+ */
+int main(void)
+{
+  	int c = 97;
+
+	while (c <= 122)
+	{
+	  	if (c == 113 || c == 101)
+		{
+		  	c++;
+		}else
+		{
+		  	putchar(c);
+		  	c++;
+		}
+	}
+	putchar(0xA);
+	return (0);
+}
