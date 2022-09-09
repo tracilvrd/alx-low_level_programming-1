db0479cd18947c9be735c24f221ecc5a22199752
cj1607 <ucheokorojefferson@gmail.com>
2022-09-09 03:01:31 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - PRogram entry point. Prints alphabets in lowercase
+ *
+ * Return: 0 id successful. Non-zero otherwise
+ */
+int main(void)
+{
+	int c = 97;
+
+	while (c <= 122)
+	{
+		putchar (c);
+		c++;
+	}
+	putchar(0xA);
+	return (0);
+}
+
+		
