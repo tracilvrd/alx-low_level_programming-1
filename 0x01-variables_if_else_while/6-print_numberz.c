6046f15aee02e099d47c87715856e1e2a5ef5987
cj1607 <ucheokorojefferson@gmail.com>
2022-09-09 12:17:26 -0700

    print nz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point. Prints 0 to 9 using putchar
+ *
+ * Return: 0 if successful. Non-zero otherwise
+ */
+int main(void)
+{
+  	int i = 0x30;
+
+	for (; i <= 0x39; i++)
+	{
+	 	 putchar(i);
+	}
+	putchar(0xA);
+	return (0x0);
+}
