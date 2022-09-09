1dd879b1a519604b1584e3af3d0412a2981a568e
cj1607 <ucheokorojefferson@gmail.com>
2022-09-09 12:31:37 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - PRogram entry point. Prints the hex alphabet in lowercase
+ *
+ * Return: 0 id successful. Non-zero otherwise
+ */
+int main(void)
+{
+  	int c = 48;
+
+	for (; c <= 57; c++)
+	{
+	  	putchar(c);
+	}
+
+	for (c += 39; c <= 102; c++)
+	{
+	  	putchar(c);
+	}
+	putchar(0xA);
+	return (0);
+}
