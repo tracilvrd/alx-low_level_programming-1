a51728182821f5f9db5797af392f7623a4fd17f8
cj1607 <ucheokorojefferson@gmail.com>
2022-09-09 12:39:14 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point - Prints possible combinations of single digits
+ *
+ * Return: 0 if succcessful, 1 otherwise
+ */
+int main(void)
+{
+  	int num = 48;
+
+	for (; num <= 57; num++)
+	{
+	  	putchar(num);
+		if (num != 57)
+		{
+		  	putchar(44);
+			putchar(32);
+		}else
+		  	break;
+	}
+	putchar(10);
+	return (0);
+}
