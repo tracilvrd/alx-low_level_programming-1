482fb45dc42f5d0c2b9a16deea8ed990dc19eee9
cj1607 <ucheokorojefferson@gmail.com>
2022-09-13 16:11:59 -0700

    add c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two numbers.
+ * @num1: First value.
+ * @num2: Second value.
+ *
+ * Return: result of num1 + num2.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - Prints out the 9 times table, but always starts with 0.
+ *
+ * Return: None
+ */
+void times_table(void)
+{
+	int row, column;
+
+	for (row = 0; row <= 9; row++)
+	{
+		_putchar('0');
+
+		for (column = 1; column <= 9; column++)
+		{
+			int multiple = row * column;
+
+			if (multiple < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + multiple);
+			}else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + multiple / 10);
+				_putchar('0' + multiple % 10);
+			}
+
+			if (column == 9)
+				_putchar('\n');
+		}
+	}
+}
+				
