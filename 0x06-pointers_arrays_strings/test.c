0a2aeea47d7c36733b2e6fa58479ceb7689084a5
cj1607 <ucheokorojefferson@gmail.com>
2022-09-21 13:50:32 -0700

    more pointers

--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -22,7 +22,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%s + %s = %s\n", n, m, res);
+		printf("%s + %s = %s\n", n, m, res);    
 	}
 	n = "1234567890";
 	m = "1";
@@ -35,6 +35,17 @@ int main(void)
 	{
 		printf("%s + %s = %s\n", n, m, res);
 	}
+	n = "999999999";
+	m = "1";
+	res = infinite_add(n, m, r2, 10);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+	    printf("%s + %s = %s\n", n, m, res);
+	}
 	res = infinite_add(n, m, r3, 11);
 	if (res == 0)
 	{
@@ -42,7 +53,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%s + %s = %s\n", n, m, res);
+ 		printf("%s + %s = %s\n", n, m, res);
 	}
-	return (0);
+ 	return (0);
 }
