f26a052cadb3470228bdc4fce5b33f7bce7f8eb3
cj1607 <ucheokorojefferson@gmail.com>
2022-09-22 19:03:35 -0700

    pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -20,7 +20,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *string);
 char *cap_string(char *string);
-char *leet(char *string)
+char *leet(char *string);
 char *rot13(char *string);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
