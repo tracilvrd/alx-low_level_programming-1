abbc9a78d5fb7d75ba5330bd60a01ad6a55837bb
cj1607 <ucheokorojefferson@gmail.com>
2022-09-26 12:49:23 -0700

    pointers leading to the end of the world

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0' && needle[i] != '\0'; i++)
 	{
-	  if (haystack[i] == needle[i])
+		if (haystack[i] == needle[i])
 			continue;
 
 		haystack++;
