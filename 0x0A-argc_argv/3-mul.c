f538b453346ca73950a3a57b1e7ca9c3bdd984a3
cj1607 <ucheokorojefferson@gmail.com>
2022-10-02 01:17:10 -0700

    argc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,8 +10,12 @@
  */
 int main(int argc, char **argv)
 {
-	while (argc--)
-		printf("%s\n", *argv++);
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
 
-	return (0);
+	printf("Error\n");
+	return (1);
 }
